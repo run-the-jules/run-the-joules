0c5707e33d6bc58edfb4e651612e38a60fa040b7
Catherine Dean <catherinemdean15@gmail.com>
2021-02-23 20:34:06 -0600

    Add edge case and sad path testing for login page
    Co-authored-by: James Fox-Collis <68164897+jlfoxcollis@users.noreply.github.com>

--- spec/features/sessions/new_spec.rb
+++ spec/features/sessions/new_spec.rb
@@ -21,44 +21,44 @@ RSpec.describe 'new user session' do
     expect(page).to have_content('You have successfully logged in')
   end
 
-  # it 'allows user access after sign in ' do
-  #   user = User.create(email: 'test5@gmail.com', password: 'test5test5')
-  #   allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-  #   user.authenticate(user.password)
-  #   visit dashboard_user_path(user)
-  #   expect(page).to have_content("Welcome, #{user.email}")
-  # end
-  #
-  # it 'will redirect the user if user is already logged in' do
-  #   user = User.create(email: 'test5@gmail.com', password: 'test5test5')
-  #   allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-  #   user.authenticate(user.password)
-  #
-  #   visit login_path
-  #   expect(current_path).to eq(dashboard_user_path(user.id))
-  #   expect(page).to have_content('You are already logged in!')
-  # end
-  #
-  #   it 'user is logged out if click_on logout' do
-  #     visit login_path
-  #
-  #     User.create(email: 'test5@gmail.com', password: 'test5test5')
-  #
-  #     fill_in 'email', with: 'test5@gmail.com'
-  #     fill_in 'password', with: 'test5test5'
-  #     click_button 'Login'
-  #     expect(page).to have_button('Log Out')
-  #     click_button 'Log Out'
-  #     expect(page).to have_content('You are logged out')
-  #   end
-  #
-  #   it 'will kick back if incorrect login' do
-  #     user = User.create(email: 'test5@gmail.com', password: 'test5test5')
-  #     visit login_path
-  #
-  #     fill_in 'email', with: 'test4@gmail.com'
-  #     fill_in 'password', with: 'test4test4'
-  #     click_button 'Login'
-  #     expect(page).to have_content('Invalid login email, or password')
-  #   end
+  it 'allows user access after sign in ' do
+    user = User.create(email: 'test5@gmail.com', password: 'test5test5', username: 'test_username')
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+    user.authenticate(user.password)
+    visit dashboard_user_path(user)
+    expect(page).to have_content("Welcome, #{user.username}")
+  end
+
+  it 'will redirect the user if user is already logged in' do
+    user = User.create(email: 'test5@gmail.com', password: 'test5test5', username: 'test_username')
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+    user.authenticate(user.password)
+
+    visit login_path
+    expect(current_path).to eq(dashboard_user_path(user.id))
+    expect(page).to have_content('You are already logged in!')
+  end
+
+  it 'user is logged out if click_on logout' do
+    visit login_path
+
+    User.create(email: 'test5@gmail.com', password: 'test5test5', username: 'test_username')
+
+    fill_in 'email', with: 'test5@gmail.com'
+    fill_in 'password', with: 'test5test5'
+    click_button 'Submit'
+    expect(page).to have_link('Log Out')
+    click_link 'Log Out'
+    expect(page).to have_content('You are logged out')
+  end
+
+  it 'will kick back if incorrect login' do
+    User.create(email: 'test5@gmail.com', password: 'test5test5', username: 'test_username')
+    visit login_path
+
+    fill_in 'email', with: 'test4@gmail.com'
+    fill_in 'password', with: 'test4test4'
+    click_button 'Submit'
+    expect(page).to have_content('Invalid login credentials')
+  end
 end
