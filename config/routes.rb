8a3978887c74b81b1a69213e79978dbf8be41551
James Fox-Collis <jlfoxcollis@gmail.com>
2021-02-24 22:30:28 -0700

    working with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_scope :user do
+    get 'users/sign_in', to: 'users/sessions#new'
+    get 'users/sign_out', to: 'users/sessions#destroy'
+  end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: %i[create destroy] do
     member { get 'dashboard' }
   end
--- db/migrate/20210223223756_create_users.rb
+++ db/migrate/20210223223756_create_users.rb
@@ -1,11 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.string :google_token
-      t.string :utility_token
       t.string :password_digest
-      t.string :email
-      t.string :username
+      t.string :email, null: false
       t.string :address
 
       t.timestamps
