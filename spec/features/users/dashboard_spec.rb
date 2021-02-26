ad257dcddaa81b8f6457206c720f1bf936e5b8d1
Robert Heath <robert.b.heathii@gmail.com>
2021-02-25 19:38:54 -0600

    Write tests for User Dashboard
    Co-authored-by: Catherine Dean <69832134+catherinemdean15@users.noreply.github.com>

---
+++ spec/features/users/dashboard_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe 'dashboard' do
+  before :each do
+    @user = User.create(email: 'test5@gmail.com', password: 'test5test5', total_points: 150, full_name: "Tim Tyrell")
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+  end
+
+  it 'takes us to the user data' do
+    visit dashboard_user_path(@user)
+
+    expect(page).to have_content(@user.total_points)
+
+  end
+end
