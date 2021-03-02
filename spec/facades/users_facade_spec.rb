f42c1e325d7e7d70a0dd930310a4c7832e56f629
James Fox-Collis <jlfoxcollis@gmail.com>
2021-03-02 08:54:43 -0700

    passing tests

--- spec/facades/users_facade_spec.rb
+++ spec/facades/users_facade_spec.rb
@@ -6,7 +6,7 @@ describe 'users facade' do
       user = User.create(email: 'test5@gmail.com', password: 'test5test5', household_size: 3, id: 1)
 
       usage_stub = File.read('spec/fixtures/usage_data.json')
-      stub_request(:get, "https://jules-api.herokuapp.com/api/v1/usages?id=#{user.id}")
+      stub_request(:get, /usages/)
         .to_return(status: 200, body: usage_stub)
 
       usage = UsersFacade.find_usage(user.id)
