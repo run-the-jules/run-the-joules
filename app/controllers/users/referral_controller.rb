81215507bc592cec99bddf936e5c77127b6b784c
James Fox-Collis <jlfoxcollis@gmail.com>
2021-03-03 23:05:06 -0700

    referral index rescue

--- app/controllers/users/referral_controller.rb
+++ app/controllers/users/referral_controller.rb
@@ -1,12 +1,10 @@
 class Users::ReferralController < ApplicationController
   before_action :check_user
   def index
-    if params[:referral]
-      begin
-        @message = UsersService.get_meters(params)
-      rescue
-        flash[:notice] = "Something went wrong"
-      end
+    begin
+      @message = UsersService.get_meters(params)
+    rescue
+      flash[:notice] = "Something went wrong"
     end
   end
 end
