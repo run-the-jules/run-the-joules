58fcf08805fd5cc4d68a088e8c204e7c1cfc52cd
awosborn <alexander.w.osborne@gmail.com>
2021-03-04 19:38:38 -0700

    Update user table and test

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_26_012122) do
+ActiveRecord::Schema.define(version: 2021_03_05_020919) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
     t.string "email"
-    t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "uid"
-    t.string "avatar_url"
     t.string "full_name"
     t.integer "total_points"
     t.integer "household_size"
